4098217adad35778fc32758a1d19beb633e308bb
Dudeiebot <ojoexcel5@gmail.com>
2023-05-27 13:46:05 +0100

    main

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/dudeiebot/fiber-crm/database"
+	"github.com/dudeiebot/fiber-crm/lead"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 )
 
-func main() {
+func setUpRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setUpRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close() //these line will happen at the end when everything is done just before the end of the program
 }
